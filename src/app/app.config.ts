3b2e4a72c3181a1f3c82f886b21c8eaa32478bb3
MBarzallo <146205152+MBarzallo@users.noreply.github.com>
2024-10-29 08:46:27 -0500

    Commit inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"parqueadero-ramirez-barzallo","appId":"1:703233743032:web:f56d1bccbc950be711bc4d","storageBucket":"parqueadero-ramirez-barzallo.appspot.com","apiKey":"AIzaSyCeRJievoh46DkoJVYm0J9B2l0upCsas7I","authDomain":"parqueadero-ramirez-barzallo.firebaseapp.com","messagingSenderId":"703233743032","measurementId":"G-STZVLSWM54"})), provideAuth(() => getAuth())]
 };
