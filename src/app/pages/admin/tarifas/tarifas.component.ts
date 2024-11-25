31884b42bf791ec36d1566947abb23f7e099c96e
jennyfer04ramirez <jr.6311.camm@gmail.com>
2024-11-24 21:23:32 -0500

    Cambios en tarifas

--- src/app/pages/admin/tarifas/tarifas.component.ts
+++ src/app/pages/admin/tarifas/tarifas.component.ts
@@ -12,6 +12,8 @@ import { RatesService } from '../../../services/rates/rates.service';
 })
 export class TarifasComponent {
   rates: any[] = [];
+  originalRates: any[] = [];
+  showSaveButton: boolean = false;
 
   constructor(private ratesService: RatesService) {}
 
@@ -22,9 +24,25 @@ export class TarifasComponent {
   async fetchRates(): Promise<void> {
     try {
       this.rates = await this.ratesService.getRates();
+      this.originalRates = JSON.parse(JSON.stringify(this.rates)); // Copiar tarifas originales
     } catch (error) {
       console.error('Error fetching rates:', error);
     }
+  }
+
+  checkChanges(): void {
+    this.showSaveButton = !this.rates.every((rate, index) =>
+      this.compareRates(rate, this.originalRates[index])
+    );
+  }
+
+  compareRates(rate1: any, rate2: any): boolean {
+    return (
+      rate1.precioPorHora === rate2.precioPorHora &&
+      rate1.precioPorDia === rate2.precioPorDia &&
+      rate1.precioPorSemana === rate2.precioPorSemana &&
+      rate1.precioPorMes === rate2.precioPorMes
+    );
   }
 
   async updateRate(index: number): Promise<void> {
@@ -40,6 +58,10 @@ export class TarifasComponent {
       alert(
         'Tarifa actualizada con éxito y aplicada a todos los espacios de estacionamiento.'
       );
+
+      // Sincronizar tarifas originales después de guardar
+      this.originalRates[index] = { ...updatedRate };
+      this.checkChanges();
     } catch (error) {
       console.error('Error actualizando tarifa:', error);
       alert('Error al actualizar la tarifa. Por favor, inténtalo nuevamente.');
